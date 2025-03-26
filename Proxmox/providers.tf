dcce232ea7d4dcc5ddb84e21b178f5d527518a3e
augustinbeeuwsaert <augustin.beeuwsaert.etu@univ-lille.fr>
2025-03-26 09:43:06 +0100

    First config

---
+++ Proxmox/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.73.2"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = var.virtual_environment_endpoint
+  username = var.virtual_environment_username
+  password = var.virtual_environment_password
+  insecure = true
+  ssh {
+    agent = true
+  }
+}
+
---
+++ Proxmox/terraform.tfvars
@@ -0,0 +1,3 @@
+virtual_environment_username = "root@pam"
+virtual_environment_password = "augustin"
+virtual_environment_endpoint = "https://192.168.50.101:8006/"
